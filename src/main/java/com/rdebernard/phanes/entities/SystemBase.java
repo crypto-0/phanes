78d4b1e36b72e3a05f004cc18db4092a255ee442
crypto-0 <yharry58@yahoo.com>
2022-11-19 04:14:08 -0500

    removed class of Entities and replace it with class of EntityQuery to help quering entities

--- src/main/java/com/rdebernard/phanes/entities/SystemBase.java
+++ src/main/java/com/rdebernard/phanes/entities/SystemBase.java
@@ -1,176 +1,73 @@
 package com.rdebernard.phanes.entities;
 import java.util.ArrayList;
-import java.util.Iterator;
-
+import java.util.HashSet;
 
 public abstract class  SystemBase{
   protected World world;
-  protected Entities entities;
   public SystemBase(World world){
     this.world = world;
-    this.entities = new Entities();
   }
   public abstract void update(float dt);
 
-  private class Entities{
-    static ArrayList<Class<? extends Component>> all;
-    static ArrayList<Class<? extends Component>> any;
-    static ArrayList<Class<? extends Component>> none;
-    public  Entities withAll(ArrayList<Class<? extends Component>> componentTypeList){
-      all = null;
-      all = componentTypeList;
-      return this;
-    }
-    public Entities withAny(ArrayList<Class<? extends Component>> componentTypeList){
-      any = null;
-      any = componentTypeList;
-      return this;
-    }
-    public  Entities withNone(ArrayList<Class<? extends Component>> componentTypeList){
-      none = null;
-      none = componentTypeList;
-      return this;
+  protected final class EntityQuery{
+    private HashSet<Entity> matchedEntities;
+    private ArrayList<Class<? extends Component>> all;
+    private ArrayList<Class<? extends Component>> any;
+    private ArrayList<Class<? extends Component>> none;
+    private 
+    EntityQuery(){
+      all = new ArrayList<>();
+      any = new ArrayList<>();
+      none = new ArrayList<>();
+      matchedEntities = new HashSet<>();
     }
     @SafeVarargs
-    final public void forEach(Job job,Class<? extends Component>... componentTypes){
-      ArrayList<Entity> queryEntities = new ArrayList<>();
-      ArrayList<EntityArchetype> entityArchetypes = new ArrayList<>();
-      if(all == null) all = new ArrayList<>();
-      for(Class<? extends Component> componentType: componentTypes){
+    final public <T extends Component> void  withAll(Class<T>... componentTypes){
+      all.clear();
+      for(Class<T> componentType: componentTypes){
         all.add(componentType);
       }
-      for(EntityArchetype entityArchetype: world.entityManager.getEntityArchetypes()){
-        if(entityArchetype.containAll(all)){
-          entityArchetypes.add(entityArchetype);
-        };
+    }
+    @SafeVarargs
+    final public <T extends Component> void  withAny(Class<T>... componentTypes){
+      any.clear();
+      for(Class<T> componentType: componentTypes){
+        any.add(componentType);
       }
-      Iterator<EntityArchetype> itr = entityArchetypes.iterator();
-      while(itr.hasNext()){
-        EntityArchetype entityArchetype = itr.next();
-        if(entityArchetype.containAny(any)){
-          itr.remove();
-        }
-        else if(!entityArchetype.containNone(none)){
-          itr.remove();
-        }
+    }
+    @SafeVarargs
+    final public <T extends Component> void  withNone(Class<T>... componentTypes){
+      none.clear();
+      for(Class<T> componentType: componentTypes){
+        none.add(componentType);
       }
-      for(EntityArchetype entityArchetype: entityArchetypes){
-        queryEntities.addAll(world.entityManager.getEntities(entityArchetype));
-      }
-      switch(componentTypes.length){
-        case 0:
-          {
-            for(Entity entity: queryEntities){
-              job.doWork(entity);
-            }
-          }
-          break;
-        case 1:
-          {
-            for(Entity entity: queryEntities){
-              job.doWork(entity,world.componentManager.getComponent(entity,componentTypes[0]));
-            }
-          }
-          break;
-        case 2:
-          {
-            for(Entity entity: queryEntities){
-              job.doWork(entity,
-                  world.componentManager.getComponent(entity,componentTypes[0])
-                  ,world.componentManager.getComponent(entity,componentTypes[1]));
-            }
-          }
-          break;
-        case 3:
-          {
-            for(Entity entity: queryEntities){
-              job.doWork(entity,
-                  world.componentManager.getComponent(entity,componentTypes[0])
-                  ,world.componentManager.getComponent(entity,componentTypes[1])
-                  ,world.componentManager.getComponent(entity,componentTypes[2]));
-            }
-          }
-          break;
-        case 4:
-          {
-            for(Entity entity: queryEntities){
-              job.doWork(entity,
-                  world.componentManager.getComponent(entity,componentTypes[0])
-                  ,world.componentManager.getComponent(entity,componentTypes[1])
-                  ,world.componentManager.getComponent(entity,componentTypes[2])
-                  ,world.componentManager.getComponent(entity,componentTypes[3]));
-            }
-          }
-          break;
-        case 5:
-          {
-            for(Entity entity: queryEntities){
-              job.doWork(entity,
-                  world.componentManager.getComponent(entity,componentTypes[0])
-                  ,world.componentManager.getComponent(entity,componentTypes[1])
-                  ,world.componentManager.getComponent(entity,componentTypes[2])
-                  ,world.componentManager.getComponent(entity,componentTypes[3])
-                  ,world.componentManager.getComponent(entity,componentTypes[4]));
-            }
-          }
-          break;
-        case 6:
-          {
-            for(Entity entity: queryEntities){
-              job.doWork(entity,
-                  world.componentManager.getComponent(entity,componentTypes[0])
-                  ,world.componentManager.getComponent(entity,componentTypes[1])
-                  ,world.componentManager.getComponent(entity,componentTypes[2])
-                  ,world.componentManager.getComponent(entity,componentTypes[3])
-                  ,world.componentManager.getComponent(entity,componentTypes[4])
-                  ,world.componentManager.getComponent(entity,componentTypes[5]));
-            }
-          }
-          break;
-        case 7:
-          {
-            for(Entity entity: queryEntities){
-              job.doWork(entity,
-                  world.componentManager.getComponent(entity,componentTypes[0])
-                  ,world.componentManager.getComponent(entity,componentTypes[1])
-                  ,world.componentManager.getComponent(entity,componentTypes[2])
-                  ,world.componentManager.getComponent(entity,componentTypes[3])
-                  ,world.componentManager.getComponent(entity,componentTypes[4])
-                  ,world.componentManager.getComponent(entity,componentTypes[5])
-                  ,world.componentManager.getComponent(entity,componentTypes[6]));
-            }
-          }
-          break;
-        default:
-          {
-            for(Entity entity: queryEntities){
-              job.doWork(entity,
-                  world.componentManager.getComponent(entity,componentTypes[0])
-                  ,world.componentManager.getComponent(entity,componentTypes[1])
-                  ,world.componentManager.getComponent(entity,componentTypes[2])
-                  ,world.componentManager.getComponent(entity,componentTypes[3])
-                  ,world.componentManager.getComponent(entity,componentTypes[4])
-                  ,world.componentManager.getComponent(entity,componentTypes[5])
-                  ,world.componentManager.getComponent(entity,componentTypes[6])
-                  ,world.componentManager.getComponent(entity,componentTypes[7]));
-            }
-          }
-          break;
-      }
+    }
+    final public void resetFilter(){
+      all.clear();
+      any.clear();
+      none.clear();
+    }
+    final public void query(){
+      matchedEntities.clear();
+      world.entityManager.getEntityArchetypes().stream().filter(entityArchetype->{
+        return entityArchetype.containAll(all);
+      }).filter(entityArchetype->{
+        return entityArchetype.containAny(any);
+      }).filter(entityArchetype->{
+        return entityArchetype.containNone(none);
+      }).forEach(entityArchetype->{
+        matchedEntities.addAll(world.entityManager.getEntities(entityArchetype));
+      });
+    }
+    final public ArrayList<Entity> getEntities(){
+      return new ArrayList<>(matchedEntities);
+    }
+    final public <T extends Component> ArrayList<T> getComponents(Class<T> componentType){
+      ArrayList<T> components = new ArrayList<>();
+      matchedEntities.forEach(e->{
+        components.add(world.componentManager.getComponent(e, componentType));
+      });
+      return components;
     }
   }
-  protected abstract class Job{
-    public void  doWork(Entity entity){}
-    public <A extends Component> void  doWork(Entity entity,A a){
-    }
-    public <A,B extends Component> void  doWork(Entity entity,A a,B b){
-    }
-    public <A,B,C extends Component> void  doWork(Entity entity,A a,B b,C c){}
-    public <A,B,C,D extends Component> void  doWork(Entity entity,A a,B b,C c,D d){}
-    public <A,B,C,D,E extends Component> void  doWork(Entity entity,A a,B b,C c,D d,E e){}
-    public <A,B,C,D,E,F extends Component> void  doWork(Entity entity,A a,B b,C c,D d,E e,F f){}
-    public <A,B,C,D,E,F,G extends Component> void  doWork(Entity entity,A a,B b,C c,D d,E e,F f,G g){}
-    public <A,B,C,D,E,F,G,H extends Component> void  doWork(Entity entity,A a,B b,C c,D d,E e,F f,G g,H h){}
-  }
-
 }
